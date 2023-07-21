038dbf12e4162b5af653b3a56fdf89712c91393a
Nikhil <nik326990@gmail.com>
2023-07-21 09:10:46 +0530

    image puzzle game

---
+++ puzzle.js
@@ -0,0 +1,64 @@
+var rows = 3;
+var columns = 3;
+
+
+var currTile;
+var otherTile;
+
+var turns = 0;
+
+var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
+
+window.onload = function () {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("img");
+            tile.id = r.toString() + c.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+
+
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
+
+            document.getElementById("board").append(tile);
+
+
+        }
+    }
+}
+
+function dragStart() {
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this;
+}
+
+function dragEnd() {
+
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+}
